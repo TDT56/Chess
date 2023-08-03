ec9af2baf2ca71d292f171231eef08c146d047fe
Tian du Toit <tianexternal@gmail.com>
2023-08-03 21:27:35 +0200

    movement file added

---
+++ movement.py
@@ -0,0 +1,20 @@
+# replacing function should be used for movement
+
+def rook():
+    pass
+
+
+def knight():
+    pass
+
+
+def bishop():
+    pass
+
+
+def king():
+    pass
+
+
+def pawn():
+    pass
