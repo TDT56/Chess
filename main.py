374359aa6232c0377e051b6e0b1926ca164f7606
Tian du Toit <tianexternal@gmail.com>
2023-08-03 21:15:28 +0200

    using a dictionary to display board

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
 from var import *
 
 
-def display_board():
-    # this is for a list, a dictionary will be used
-    r = ['', '', '', '', '', '', '', '', '']
-    board_letters = '       a      b      c      d      e      f      g      h'
-    board_line = '    ---------------------------------------------------------'
-    board_squares = f'{r[0]} |  {r[1]}  |  {r[2]}  |  {r[3]}  |  {r[4]}  |  {r[5]}  |  {r[6]}  |  {r[7]}  |  {r[8]}  |'
-
-    board = starting_board
+def display_board(board):
     print(board_letters)
     print(board_line)
-    for row in range(16):
-        if row % 2:
-            print(board_line)
-        else:
-            location = 8 * int(row / 2)
-            r3 = ''
-            for num, value in enumerate(board[location:location+8]):
-
-                if value == '':
-                    value = '  '  # ▭, ―
-                r2 = f'  {value}  |'
-                if num != 0:
-                    r3 = r3 + r2
-                else:
-                    r1 = f' {int((row/2)+1)}  |'
-                    r3 = r1 + r2
-            print(r3)
+    for num in num_list[::-1]:
+        num = str(num)
+        r3 = num + '  |'
+        for letter in abc_list:
+            try:
+                r2 = f'  {board[letter+num]}   |'
+            except KeyError:
+                r2 = f'     |'
+            r3 = r3 + r2
+        print(r3)
+        print(board_line)
 
 
 if __name__ == '__main__':
-    print_hi()
+    display_board(starting_board)
