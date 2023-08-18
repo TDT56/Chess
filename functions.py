7322abc1ce899da505b2561521092a21f8ccb4b8
Tian du Toit <tianexternal@gmail.com>
2023-08-18 12:01:14 +0200

    correct rooks movement for both colours

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ def selecting_chess_piece():
     """
     x = input('Enter the coordinates of the piece you would like to select: ')
     x = x[0].upper() + x[1]
-    print(f'You have selected {x}')
+    print(f'{x} - ', end='')
     return x
 
 
@@ -21,8 +21,8 @@ def display_board(board):
             # The try and except is only when the actual chess icons is used
             try:
                 r2 = f'  {board[letter+num][0]}   |'
-            except KeyError:
-                r2 = f'        |'
+            except IndexError:
+                r2 = f'      |'
             r3 = r3 + r2
         print(r3)
         print(board_line)
@@ -31,7 +31,7 @@ def display_board(board):
 def valid_selection(xy: str, board: dict):
     """Validate the user selection"""
     if xy in board_keys:
-        if board[xy][0] in piece_ids:
+        if board[xy][0].upper() in piece_ids:
             return True
         else:
             print('You selected an EMPTY space, try again.')
--- movement.py
+++ movement.py
@@ -1,6 +1,6 @@
 # replacing function should be used for movement
 from var import *
-from functions import valid_selection
+from functions import valid_selection, display_board
 
 
 class PieceMovement:
@@ -9,33 +9,32 @@ class PieceMovement:
         self.board = board
         self.x = xy[0]
         self.y = str(xy[1])
-        self.empty = ' '
+        self.empty = '  '
         self.movable_coordinates = []
 
     def movement(self):
         """xy: coordinates"""
-        print('Execute movement function') if comments else None
         if valid_selection(self.xy, self.board):
             piece_type = self.board[self.xy][0].upper()
 
             if piece_type == 'P':
-                print('P')
+                print(piece_ids[piece_type])
                 pass
             elif piece_type == 'N':
-                print('N')
+                print(piece_ids[piece_type])
                 pass
             elif piece_type == 'B':
-                print('P')
+                print(piece_ids[piece_type])
                 pass
             elif piece_type == 'R':
-                print('R')
+                print(piece_ids[piece_type])
                 self.rook()
             elif piece_type == 'Q':
-                print('Q')
+                print(piece_ids[piece_type])
                 pass
             # else King
             else:
-                print('K')
+                print(piece_ids[piece_type])
                 pass
         else:
             print('Movement function unsuccessful validation selection.')
@@ -46,45 +45,42 @@ class PieceMovement:
         if self.board[coordinates] == self.empty:
             self.movable_coordinates.append(coordinates)
             return False
-        elif (self.board[coordinates][0].isupper() and self.x.islower()) or \
-                (self.board[coordinates][0].islower() and self.x.isupper()):
+        elif (self.board[coordinates][0].isupper() and self.board[self.xy].islower()) or \
+                (self.board[coordinates][0].islower() and self.board[self.xy].isupper()):
             self.movable_coordinates.append(coordinates)
-            return False
+            return True
         else:
             return True
 
     def rook(self):
-
-        movable_coordinates = []
-        print('Entered the rook')
         abc_index = abc_tup.index(self.xy[0])
         num_index = num_tup.index(int(self.xy[1]))
-
         # Stepping in abc+ direction
         for i, abc in enumerate(abc_tup[abc_index + 1:]):
             coordinates = abc + self.y
             if self.calculate_direction_movement(coordinates):
                 break
-
         # Stepping in abc- direction
         for i, abc in enumerate(reversed(abc_tup[0:abc_index])):
             coordinates = abc + self.y
             if self.calculate_direction_movement(coordinates):
                 break
-
         # Stepping in num+ direction
         for i, num in enumerate(num_tup[num_index + 1:]):
             coordinates = self.x + str(num)
             if self.calculate_direction_movement(coordinates):
                 break
-
         # Stepping in num- direction
         for i, num in enumerate(reversed(num_tup[0:num_index])):
             coordinates = self.x + str(num)
             if self.calculate_direction_movement(coordinates):
                 break
 
-        print(self.movable_coordinates)
+        # PRINT MOVABLE COORDINATES
+        new_board = empty_board
+        for xy_move in self.movable_coordinates:
+            new_board[xy_move] = 'X'
+        display_board(empty_board)
 
     def knight(self):
         pass
